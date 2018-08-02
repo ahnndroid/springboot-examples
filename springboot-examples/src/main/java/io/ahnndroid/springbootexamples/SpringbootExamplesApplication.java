be13bd8edf06a1a77586b2c7d2d4d8eafc5f002c
DESKTOP-2Q1MURN\ahnndroid <super702@gmail.com>
2018-08-02 19:58:16 +0900

    Initial commit

---
+++ springboot-examples/src/main/java/io/ahnndroid/springbootexamples/SpringbootExamplesApplication.java
@@ -0,0 +1,12 @@
+package io.ahnndroid.springbootexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootExamplesApplication.class, args);
+	}
+}
